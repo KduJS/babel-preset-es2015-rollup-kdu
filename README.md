be44944a28ffefed5e3ae959b8fe4d0694c8c699
NKDuy <kn145660@gmail.com>
2022-07-09 17:10:33 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# babel-preset-es2015-rollup-kdu
+
+A preset used for building Kdu.js source.
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = require('modify-babel-preset')('es2015', {
+  // remove commonjs transform
+  'transform-es2015-modules-commonjs': false,
+  // remove typeof symbol
+  'transform-es2015-typeof-symbol': false,
+  // add external helpers
+  'external-helpers': true,
+  // use simple class transform (loose + without callcheck)
+  'transform-es2015-classes': false,
+  'transform-es2015-classes-simple': true
+})
